f90c558c8f76c9e41bcaa4833e23523039c936a6
--replace-all <venkateshchowdary117@gmail.com>
2021-10-27 11:44:34 +0530

    updated test case for pytest in titanic survival use case

--- titanic-disaster-survival/section-05-production-model-package/tests/test_features.py
+++ titanic-disaster-survival/section-05-production-model-package/tests/test_features.py
@@ -1,17 +1,15 @@
 from classification_model.config.core import config
-from classification_model.processing.features import TemporalVariableTransformer
+from classification_model.processing.features import ExtractLetterTransformer
 
 
-def test_temporal_variable_transformer(sample_input_data):
+def test_extract_letter_transformer(sample_input_data):
     # Given
-    transformer = TemporalVariableTransformer(
-        variables=config.model_config.temporal_vars,  # YearRemodAdd
-        reference_variable=config.model_config.ref_var,
+    transformer = ExtractLetterTransformer(
+        variables=config.model_config.categorical_vars_with_extraction
     )
-    assert sample_input_data["YearRemodAdd"].iat[0] == 1961
 
     # When
     subject = transformer.fit_transform(sample_input_data)
 
     # Then
-    assert subject["YearRemodAdd"].iat[0] == 49
+    assert len(max(subject['Cabin'].astype(str).unique(), key = len))<=4
