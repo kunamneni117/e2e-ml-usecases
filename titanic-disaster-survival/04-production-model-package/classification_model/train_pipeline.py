e142932b70634eaa1418aac3a79eaded9e9358d8
--replace-all <venkateshchowdary117@gmail.com>
2021-10-28 15:03:02 +0530

    updating training model with total data rather than a subset

--- titanic-disaster-survival/04-production-model-package/classification_model/train_pipeline.py
+++ titanic-disaster-survival/04-production-model-package/classification_model/train_pipeline.py
@@ -10,18 +10,18 @@ def run_training() -> None:
     # read training data
     data = load_dataset(file_name=config.app_config.training_data_file)
 
-    # divide train and test
-    X_train, X_test, y_train, y_test = train_test_split(
-        data[config.model_config.features],  # predictors
-        data[config.model_config.target],
-        test_size=config.model_config.test_size,
-        # we are setting the random seed here
-        # for reproducibility
-        random_state=config.model_config.random_state,
-    )
+    # # divide train and test
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     data[config.model_config.features],  # predictors
+    #     data[config.model_config.target],
+    #     test_size=config.model_config.test_size,
+    #     # we are setting the random seed here
+    #     # for reproducibility
+    #     random_state=config.model_config.random_state,
+    # )
 
     # fit model
-    survival_pipe.fit(X_train, y_train)
+    survival_pipe.fit(data[config.model_config.features], data[config.model_config.target])
 
     # persist trained model
     save_pipeline(pipeline_to_persist=survival_pipe)
